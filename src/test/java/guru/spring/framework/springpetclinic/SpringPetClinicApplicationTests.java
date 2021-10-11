f2d4facd9829869ed8246ef784826ad3cea0e1d9
Raul Goes <raul.goes-ext@ab-inbev.com>
2021-10-11 10:27:23 -0300

    Initial Commit

---
+++ src/test/java/guru/spring/framework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.framework.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
