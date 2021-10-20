1a546efebe7e63b1f79c5daab3ed9cc06ae24636
Raul Goes <raul.goes-ext@ab-inbev.com>
2021-10-20 07:15:13 -0300

    Adding Pojos

---
+++ src/main/java/guru/spring/framework/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.spring.framework.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/spring/framework/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.spring.framework.springpetclinic.model;
+
+public class Vet extends Person {
+}
